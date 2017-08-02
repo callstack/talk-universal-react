8fe762cf26d4dba4dbe2d0b68967b4d8e3da0db5
Michal Chudziak <michal.chudziak@gmail.com>
2017-08-02 12:51:04 +0200

    Fix boilerplate.

--- images.js
+++ images.js
@@ -4,10 +4,6 @@ const images = {
   logo: require('assets/img/logo-blue.svg'),
   background: require('assets/img/bg.jpg'),
   mike: require('assets/img/mike.jpg'),
-  wojcio: require('assets/img/wojcio.png'),
-  dirtyTree: require('assets/img/dirtyTree.png'),
-  drunk: require('assets/img/drunk.jpg'),
-  fight: require('assets/img/fight.jpg'),
 };
 
 preloader(images);
